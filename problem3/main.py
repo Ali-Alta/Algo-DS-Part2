9913880b0eb5022154a3abea67a2b7c1de8019fb
alisofyansaputra <Buffware4@gmail.com>
2024-03-09 21:25:52 +0700

    problem3.main.py

--- problem3/main.py
+++ problem3/main.py
@@ -1,4 +1,21 @@
 def playing_domino(cards, deck):
+    for card in cards:
+        if card[0] == deck[0]:
+            deck.insert(0, card[0])
+            cards.remove(card)
+            return deck
+        elif card[0] == deck[0]:
+            deck.insert(0, card[0])
+            cards.remove(card)
+            return deck
+        elif card[0] == deck[-1]:
+            deck.append(card[1])
+            cards.remove(card)
+            return deck
+        elif card[0] == deck[-1]:
+            deck.append(card[0])
+            cards.remove(card)
+            return deck
     return []
 
 if __name__ == "__main__":
